fd9590ed4ebbbf1fa22349e96ed34e539ab31f15
purpleclay <purpleclaygh@gmail.com>
2023-03-07 10:30:32 +0000

    fix windows tests

--- gittest/repository.go
+++ gittest/repository.go
@@ -282,7 +282,9 @@ func changeToDir(t *testing.T, dir string) string {
 }
 
 func cloneRemoteAndInit(t *testing.T, cloneName string, options ...string) {
-	MustExec(t, fmt.Sprintf("git clone %s file://$(pwd)/%s %s", strings.Join(options, " "), BareRepositoryName, cloneName))
+	url := filepath.Join("$(pwd)", BareRepositoryName)
+
+	MustExec(t, fmt.Sprintf("git clone %s file://%s %s", strings.Join(options, " "), url, cloneName))
 	require.NoError(t, os.Chdir(cloneName))
 
 	// Ensure author details are set
